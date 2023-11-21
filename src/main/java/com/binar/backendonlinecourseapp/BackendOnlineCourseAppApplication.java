f0c4607a46cf6031f369d2766a91207df0f56093
Demonicky <edotank47@gmail.com>
2023-11-21 11:20:12 +0700

    add new project

---
+++ src/main/java/com/binar/backendonlinecourseapp/BackendOnlineCourseAppApplication.java
@@ -0,0 +1,13 @@
+package com.binar.backendonlinecourseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendOnlineCourseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendOnlineCourseAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/binar/backendonlinecourseapp/BackendOnlineCourseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binar.backendonlinecourseapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendOnlineCourseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
