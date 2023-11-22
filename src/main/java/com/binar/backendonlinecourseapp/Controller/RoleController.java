4721fe2427902479aaec77f76b2d23d63fd3af66
Nur Ainun <ainun9874@gmail.com>
2023-11-22 11:54:36 +0700

    role (update & delete)

--- src/main/java/com/binar/backendonlinecourseapp/Controller/RoleController.java
+++ src/main/java/com/binar/backendonlinecourseapp/Controller/RoleController.java
@@ -1,14 +1,12 @@
 package com.binar.backendonlinecourseapp.Controller;
 
+import com.binar.backendonlinecourseapp.Entity.Role;
 import com.binar.backendonlinecourseapp.Service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/role")
@@ -23,6 +21,17 @@ public class RoleController {
     public ResponseEntity<String> createRole(@RequestParam String roleName){
         String response = roleService.createRole(roleName);
         return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<String> updateRole(@RequestBody Role role) {
+        String response = roleService.updateRole(role);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+    @DeleteMapping("/delete/{roleId}")
+    public ResponseEntity<String> deleteRole(@PathVariable Long roleId) {
+        roleService.deleteRole(roleId);
+        return ResponseEntity.ok("Role deleted successfully");
     }
 
 }
