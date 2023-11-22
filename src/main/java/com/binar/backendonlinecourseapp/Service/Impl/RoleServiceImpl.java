2cc665acb55bbdbc21e660a85312760d30328c7a
Nur Ainun <ainun9874@gmail.com>
2023-11-22 12:35:59 +0700

    role (update & delete)

--- src/main/java/com/binar/backendonlinecourseapp/Service/Impl/RoleServiceImpl.java
+++ src/main/java/com/binar/backendonlinecourseapp/Service/Impl/RoleServiceImpl.java
@@ -19,4 +19,20 @@ public class RoleServiceImpl implements RoleService {
         roleRepository.save(role);
         return "berhasil";
     }
+
+    @Override
+    public String updateRole(Role role) {
+        Role upRole = roleRepository.findById(role.getId())
+                .orElseThrow(() -> new RuntimeException("Role not found"));
+        upRole.setRoleName(role.getRoleName());
+        roleRepository.save(upRole);
+        return "Success";
+    }
+
+    @Override
+    public void deleteRole(Long roleId) {
+        Role role = roleRepository.findById(roleId)
+                .orElseThrow(() -> new RuntimeException("Role not found"));
+        roleRepository.delete(role);
+    }
 }
