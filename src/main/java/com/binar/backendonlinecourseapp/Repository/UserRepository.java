a315ab40fc06eb6eae3338769ea2fa10828cf063
Demonicky <edotank47@gmail.com>
2023-12-04 14:43:18 +0700

    awmaomda

--- src/main/java/com/binar/backendonlinecourseapp/Repository/UserRepository.java
+++ src/main/java/com/binar/backendonlinecourseapp/Repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.binar.backendonlinecourseapp.Repository;
 
 import com.binar.backendonlinecourseapp.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -13,5 +15,7 @@ public interface UserRepository extends JpaRepository<User, String> {
 
     Optional<User> findByTelp(String telp);
 
+    @Query("SELECT u FROM User u WHERE u.email = :identifier OR u.telp = :identifier")
+    Optional<User> findUserByEmailOrtelp(@Param("identifier") String identifier);
 
 }
