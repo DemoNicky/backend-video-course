bd5a434045c0226cdb49553903284506587a2bf8
Demonicky <edotank47@gmail.com>
2023-12-08 21:39:52 +0700

    update cors

--- src/main/java/com/binar/backendonlinecourseapp/BackendOnlineCourseAppApplication.java
+++ src/main/java/com/binar/backendonlinecourseapp/BackendOnlineCourseAppApplication.java
@@ -18,7 +18,8 @@ public class BackendOnlineCourseAppApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:5173");
+				registry.addMapping("/**").allowedOrigins("http://localhost:5173")
+						.allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("*").allowCredentials(true);
 			}
 		};
 	}
